e4f3ab86f754b3491b417c5f222a009d5f57fed7
khanali454 <khanandkhan061@gmail.com>
2023-11-07 12:28:28 +0500

    update

--- admin/published.php
+++ admin/published.php
@@ -14,84 +14,82 @@
     <link rel="stylesheet" href="css/user_profile.css">
     <link rel="stylesheet" href="css/published.css">
     <link rel="stylesheet" href="css/loading_animation.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
-        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 </head>
 
 <body>
-<?php include "dashboard_header.php" ?>
+    <?php include "dashboard_header.php" ?>
 
 
-                
-                <div class="_profile_heading_">Published</div>
-                <div class="_websites_stats_container published_websites_stats_container">
+
+    <div class="_profile_heading_">Published</div>
+    <div class="_websites_stats_container published_websites_stats_container">
 
 
-                      <!--stat box -->
-                      <div class="_stat_box published_space_card">
-                        <div class="_stat_box_top">
-                            <div class="stat_box_top_left">
-                                <i class="fa-solid fa-file-pen"></i>
-                            </div>
-                            <div class="stat_box_top_right">
-                                <div class="_stat_count"><?= CountPublishedBlogs() ?></div>
-                                <div class="_stat_count_name">Total Blogs</div>
-                            </div>
-                        </div>
-                        <a href="blogs.php">
-                        <div class="_stat_box_bottom">
-                            <div class="_stat_count_type_name">View</div>
-                            <i class="fa-solid fa-circle-arrow-right"></i>
-                        </div>
-                         </a>
-                    </div>
-                    <!-- stat box end -->
-
-                         <!--stat box -->
-                         <div class="_stat_box published_space_card">
-                        <div class="_stat_box_top">
-                            <div class="stat_box_top_left">
-                                <i class="fa-solid fa-file-pen"></i>
-                            </div>
-                            <div class="stat_box_top_right">
-                                <div class="_stat_count"><?= CountPublishedArticles() ?></div>
-                                <div class="_stat_count_name">Total Articles</div>
-                            </div>
-                        </div>
-                        <a href="articles.php">
-                        <div class="_stat_box_bottom">
-                            <div class="_stat_count_type_name">View</div>
-                            <i class="fa-solid fa-circle-arrow-right"></i>
-                        </div>
-                         </a>
-                    </div>
-                    <!-- stat box end -->
-                    
-                    
-                    
-                    <!--stat box -->
-                    <div class="_stat_box published_space_card">
-                   <div class="_stat_box_top">
-                       <div class="stat_box_top_left">
-                           <i class="fa-solid fa-file-pen"></i>
-                       </div>
-                       <div class="stat_box_top_right">
-                           <div class="_stat_count">110</div>
-                           <div class="_stat_count_name">Total Posts</div>
-                       </div>
-                   </div>
-                   <a href="">
-                   <div class="_stat_box_bottom">
-                       <div class="_stat_count_type_name">View</div>
-                       <i class="fa-solid fa-circle-arrow-right"></i>
-                   </div>
-                    </a>
-               </div>
-               <!-- stat box end -->
-
+        <!--stat box -->
+        <div class="_stat_box published_space_card">
+            <div class="_stat_box_top">
+                <div class="stat_box_top_left">
+                    <i class="fa-solid fa-file-pen"></i>
                 </div>
+                <div class="stat_box_top_right">
+                    <div class="_stat_count">12</div>
+                    <div class="_stat_count_name">Total Blogs</div>
+                </div>
+            </div>
+            <a href="blogs.php">
+                <div class="_stat_box_bottom">
+                    <div class="_stat_count_type_name">View</div>
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                </div>
+            </a>
+        </div>
+        <!-- stat box end -->
+
+        <!--stat box -->
+        <div class="_stat_box published_space_card">
+            <div class="_stat_box_top">
+                <div class="stat_box_top_left">
+                    <i class="fa-solid fa-file-pen"></i>
+                </div>
+                <div class="stat_box_top_right">
+                    <div class="_stat_count"><?= 0 ?></div>
+                    <div class="_stat_count_name">Total Articles</div>
+                </div>
+            </div>
+            <a href="articles.php">
+                <div class="_stat_box_bottom">
+                    <div class="_stat_count_type_name">View</div>
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                </div>
+            </a>
+        </div>
+        <!-- stat box end -->
+
+
+
+        <!--stat box -->
+        <div class="_stat_box published_space_card">
+            <div class="_stat_box_top">
+                <div class="stat_box_top_left">
+                    <i class="fa-solid fa-file-pen"></i>
+                </div>
+                <div class="stat_box_top_right">
+                    <div class="_stat_count">110</div>
+                    <div class="_stat_count_name">Total Posts</div>
+                </div>
+            </div>
+            <a href="">
+                <div class="_stat_box_bottom">
+                    <div class="_stat_count_type_name">View</div>
+                    <i class="fa-solid fa-circle-arrow-right"></i>
+                </div>
+            </a>
+        </div>
+        <!-- stat box end -->
+
+    </div>
 
 
 
@@ -99,8 +97,8 @@
 
 
 
-                <!-- CONTENT -->
-          
-<?php
-include "dashboard_footer.php";
-?>
+    <!-- CONTENT -->
+
+    <?php
+    include "dashboard_footer.php";
+    ?>
