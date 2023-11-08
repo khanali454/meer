31c54cb48e8321a39f7bf1ee681cb7d7b43bd03d
khanali454 <khanandkhan061@gmail.com>
2023-11-08 14:56:10 +0500

    update

--- admin/config.php
+++ admin/config.php
@@ -3,7 +3,8 @@ session_start();
 $db_user = "u478333117_meer_m";
 $db_password="12345678Meer";
 $db_name="u478333117_meer_m";
-
+// time config
+date_default_timezone_set("Asia/Kolkata");
 $conn = mysqli_connect("localhost",$db_user,$db_password,$db_name);
 
 
